aa7e3bb277788f9df55596e47ba7d58b59ee61c6
DEVESH JETHWA <153332864+Djethwa1@users.noreply.github.com>
2024-02-09 12:38:51 +0000

    Add files via upload
    CODE written for task 1

---
+++ details.py
@@ -0,0 +1,6 @@
+# This code requests user input for their name, age and address and prints all these details on one line
+name = input("What is your name:")
+age = input("What is your age:")
+house_number = input("What is your house number:")
+street_name = input("What is the name of your street:")
+print(f"This is {name}, he is {age} years old, and lives at {house_number}, {street_name}")
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+#This code will use the input comand requesting the user to enter their name and age which will be stored in respective variables (name and age), the program will then print these values and finally the program will print Hello world
+name = input("Please enter your Name:")
+print(name)
+age = input("What is your age:")
+print(age)
+print("Hello World!")
